297fe75f278695bb50b49b41cd38171671fe96dc
Martin <martin.abilev@gmail.com>
2024-08-24 13:51:35 +0300

    8x8

--- src/bugsconfig.zig
+++ src/bugsconfig.zig
@@ -2,5 +2,5 @@
 pub const maxBugs: usize = 8;
 pub const maxIn: usize = 7;
 pub const maxOut: usize = 6;
-pub const maxHidden: usize = 254;
+pub const maxHidden: usize = 8 * 8;
 pub const maxCons: usize = maxHidden;
